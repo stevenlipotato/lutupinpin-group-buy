ab6c89c50c8f1d7b54692e157e348f3a7f33d54f
steven <stevenlyc@192.168.1.238>
2025-02-04 02:27:48 +0000

    Restructure project with new entry point

---
+++ lutou-group/src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app; 
